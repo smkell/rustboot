ebf4d41329c416899e37b8cc801c7733a81d4e86
Piotr Czarnecki <pioczarn@gmail.com>
2014-02-20 18:17:24 +0100

    Fix to_str_bytes

--- kernel/int.rs
+++ kernel/int.rs
@@ -1,3 +1,5 @@
+use core::fail::assert;
+
 pub fn range(lo: uint, hi: uint, it: |uint|) {
     let mut iter = lo;
     while iter < hi {
@@ -6,25 +8,40 @@ pub fn range(lo: uint, hi: uint, it: |uint|) {
     }
 }
 
+macro_rules! int_module (($T:ty, $bits:expr) => (
+
 #[inline]
-pub fn to_str_bytes(num: int, radix: int, f: |u8|) {
+pub fn to_str_bytes(num: $T, radix: uint, f: |u8|) {
+    assert(2 <= radix && radix <= 36);
     let neg = num < 0;
-    let mut deccum = if neg { -num } else { num };
 
     // Radix can be as low as 2, so we need 64 characters for a number
     // near 2^64, plus another one for a possible '-' character.
     let mut buf = [0u8, ..65];
     let mut cur = 0;
 
+    // TODO: test overflow
+    let mut deccum = num / radix as $T;
+    let mut digit = num % radix as $T;
+
+    // Calculate the absolute value after dividing the whole number once,
+    // because a U2 representable negative number doesn't necessarily have
+    // a unique inverse of the same type.
+    // Example: -128i8 == 128i8. For radix=2, -128i8/2 = -64i8 != 64i8.
+    if neg {
+        deccum = -deccum;
+        digit = -digit;
+    }
+
     loop {
-        let digit = deccum % radix;
         buf[cur] = match digit as u8 {
             i @ 0..9 => '0' as u8 + i,
             i        => 'a' as u8 + (i-10),
         };
         cur += 1;
-        deccum /= radix as int;
+        deccum /= radix as $T;
         if deccum == 0 { break; }
+        digit = deccum % radix as $T;
     }
 
     if neg {
@@ -36,3 +53,8 @@ pub fn to_str_bytes(num: int, radix: int, f: |u8|) {
         f(buf[cur]);
     }
 }
+
+))
+
+#[cfg(target_word_size = "32")] int_module!(int, 32)
+#[cfg(target_word_size = "64")] int_module!(int, 64)
