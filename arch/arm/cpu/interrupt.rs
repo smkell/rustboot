715b12911d794324f858c0cbf446248578fcfd8d
Piotr Czarnecki <pioczarn@gmail.com>
2014-05-28 14:16:26 +0200

    arm: Update

--- arch/arm/cpu/interrupt.rs
+++ arch/arm/cpu/interrupt.rs
@@ -1,5 +1,7 @@
-use core::mem::{volatile_store, transmute};
-use core::intrinsics::offset;
+use core::intrinsics::{offset, transmute, volatile_store};
+
+use core::failure;
+use core::fmt;
 
 use platform::io;
 
@@ -83,6 +85,14 @@ extern {
 #[no_mangle]
 pub unsafe fn debug() {
     asm!("movs pc, lr")
+}
+
+// TODO respect destructors
+#[lang="begin_unwind"]
+extern "C" fn begin_unwind(fmt: &fmt::Arguments, file: &str, line: uint) -> ! {
+    unsafe {
+        loop { };
+    };
 }
 
 /*
