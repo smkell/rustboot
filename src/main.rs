e61d67f2addb8eb28d4bb30748af7c98d871c330
Kell, Sean <skell@ea.com>
2015-12-21 17:25:28 -0500

    Move VGA code to its own module.

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,9 @@
 #![feature(no_std)]
-
 #![no_std]
 
-enum Color {
-    Black      = 0,
-    Blue       = 1,
-    Green      = 2,
-    Cyan       = 3,
-    Red        = 4,
-    Pink       = 5,
-    Brown      = 6,
-    LightGray  = 7,
-    DarkGray   = 8,
-    LightBlue  = 9,
-    LightGreen = 10,
-    LightCyan  = 11,
-    LightRed   = 12,
-    LightPink  = 13,
-    Yellow     = 14,
-    White      = 15,
-}
-
-fn clear_screen(background: u16) {
-    let max = 80 * 25;
-
-    for i in 0..max {
-        unsafe {
-            *((0xb8000 + i * 2) as *mut u16) = (background as u16) << 12;
-        }
-    }
-}
+mod vga;
 
 #[no_mangle]
 pub fn main() {
-    clear_screen(Color::LightCyan as u16);
+    vga::clear_screen(vga::Color::LightCyan as u16);
 }
---
+++ src/vga.rs
@@ -0,0 +1,28 @@
+pub enum Color {
+    Black      = 0,
+    Blue       = 1,
+    Green      = 2,
+    Cyan       = 3,
+    Red        = 4,
+    Pink       = 5,
+    Brown      = 6,
+    LightGray  = 7,
+    DarkGray   = 8,
+    LightBlue  = 9,
+    LightGreen = 10,
+    LightCyan  = 11,
+    LightRed   = 12,
+    LightPink  = 13,
+    Yellow     = 14,
+    White      = 15,
+}
+
+pub fn clear_screen(background: u16) {
+    let max = 80 * 25;
+
+    for i in 0..max {
+        unsafe {
+            *((0xb8000 + i * 2) as *mut u16) = (background as u16) << 12;
+        }
+    }
+}
