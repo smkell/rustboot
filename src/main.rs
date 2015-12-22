e0b5ac475df68eda38714da6069ce1b0e9a012c5
Kell, Sean <skell@ea.com>
2015-12-22 10:27:40 -0500

    Implement write_char function.
    Allows us to write single characters to the terminal.

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,6 @@ mod vga;
 #[no_mangle]
 pub fn main() {
     vga::clear_screen(vga::Color::LightCyan as u16);
+    vga::write_char('n', 0, 0);
+    vga::write_char('e', 1, 0);
 }
--- src/vga.rs
+++ src/vga.rs
@@ -27,3 +27,72 @@ pub fn clear_screen(background: u16) {
         }
     }
 }
+
+struct Entry {
+    data: u8,
+    attrib: u8,
+}
+
+impl Entry {
+    fn new(c: char, bg: Color, fg: Color) -> Entry {
+        Entry{ data: c as u8, attrib: ((bg as u8) << 4) | ((fg as u8) & 0x0F) }
+    }
+
+    fn from_raw(raw: u16) -> Entry {
+        let data: u8 = (raw & 0x00FF) as u8;
+        let attrib: u8 = ((raw & 0xFF00) >> 8) as u8;
+
+        Entry { data: data, attrib: attrib }
+    }
+
+    fn raw(self) -> u16 {
+        self.data as u16 | ((self.attrib as u16) << 8)
+    }
+}
+
+#[test]
+fn entry_from_raw_test() {
+    let bg = Color::LightCyan;
+    let fg = Color::Black;
+    let c: char = 'n';
+
+    let entry = Entry::new(c, bg, fg);
+    assert_eq!(0x6E, entry.data);
+    assert_eq!(0xB0, entry.attrib);
+
+    let raw = entry.raw();
+    assert_eq!(0xB06E, raw);
+
+    let entry = Entry::from_raw(raw);
+    assert_eq!(0x6E, entry.data);
+    assert_eq!(0xB0, entry.attrib);
+}
+
+pub fn write_char(c: char, x: usize, y: usize) {
+    // Get the current state of the entry.
+    let mut entry = get_entry(x, y);
+
+    // Update the character data.
+    entry.data = c as u8;
+
+    write_entry(entry, x, y);
+}
+
+fn write_entry(entry: Entry, x: usize, y: usize) {
+    let pos = x + y * 80;
+
+    unsafe {
+        *((0xb8000 + pos*2) as *mut u16) = entry.raw();
+    }
+}
+
+fn get_entry(x: usize, y: usize) -> Entry {
+    let raw: u16;
+    let pos = x + y * 80;
+
+    unsafe {
+        raw = *((0xb8000 + pos*2) as *mut u16);
+    }
+
+    Entry::from_raw(raw)
+}
